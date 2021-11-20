922ec3d4f7c02055132be2507bbf00a00b88c463
Yarob Al Mostafa (يعرُب) <yarob.hm@gmail.com>
2021-11-20 11:58:53 +0300

    final changes

--- SegueAndScreensTransitions/ViewController.swift
+++ SegueAndScreensTransitions/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
 
+    @IBAction func nextScreenButtonClicked(_ sender: Any)
+    {
+        var title = inputTextField.text!
+        if title == "الصفحة الثانية" {
+            let vc = storyboard?.instantiateViewController(withIdentifier: "secondVC")
+            self.present(vc!, animated: true, completion: nil)
+        }else if title == "الصفحة الثالثة" {
+            let vc = storyboard?.instantiateViewController(withIdentifier: "thirdVC")
+            present(vc!, animated: true, completion: nil)
+        }
+        else {
+            print("don't move")
+        }
+    }
+    
 }
 
